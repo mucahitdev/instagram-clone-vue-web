2d52280b838ed13b6f0205bfe8c6aafcd1c2cc9b
mucahitkok <bv3ne67bvv>
2021-03-11 06:17:06 +0300

    css ve bazı styles ayarları yapıldı

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'src/styles/app.css'
 
 Vue.config.productionTip = false
-
+// todo adjbuıdbaw
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <About msg="Welcome to Your Vue.js App" />
+    <ctext msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import About from '/src/views/About'
-
+import ctext from '@/components/ctext'
 export default {
   name: 'Home',
   components: {
-    About
+    ctext : ctext
   }
 }
 </script>
