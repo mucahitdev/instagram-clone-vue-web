36b71dfdf5c2e6392bec60319def648a6fb541eb
mucahitkok <bv3ne67bvv>
2021-03-11 05:40:52 +0300

    ilk commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <About msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import About from '/src/views/About'
+
+export default {
+  name: 'Home',
+  components: {
+    About
+  }
+}
+</script>
